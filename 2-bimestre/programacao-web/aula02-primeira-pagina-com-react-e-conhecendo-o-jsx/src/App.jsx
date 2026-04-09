fa71057b61ef7cc615a7460a10b6286e56c38e2d
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2026-04-09 14:55:51 +0000

    feat: aula 2 adidionada

---
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -0,0 +1,24 @@
+// Crie sua solução aqui
+
+function App() {
+
+  // Criando uma variável com JavaScript
+  const filmeFavorito = "Matrix"
+
+  return (
+    <div>
+
+      {/* Nome do aluno */}
+      <h1>Olá, João</h1>
+
+      {/* Matéria favorita */}
+      <p>Minha matéria favorita é Programação</p>
+
+      {/* Variável no JSX */}
+      <p>Meu filme favorito é: {filmeFavorito}</p>
+
+    </div>
+  )
+}
+
+export default App
---
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/test/App.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react"
+import { describe, test, expect } from "vitest"
+import App from "../App"
+
+describe("App", () => {
+    test("renderiza um título h1", () => {
+        render(<App />)
+
+        const titulo = screen.getByRole("heading")
+        expect(titulo).toBeTruthy()
+    })
+
+    test("exibe matéria favorita", () => {
+        render(<App />)
+
+        const texto = screen.getByText(/matéria favorita/i)
+        expect(texto).toBeTruthy()
+    })
+
+    test("exibe filme favorito", () => {
+        render(<App />)
+
+        const filme = screen.getByText(/filme favorito/i)
+        expect(filme).toBeTruthy()
+    })
+
+})
