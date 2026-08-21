af8da5af5ddae4b9f7183197fd5153d7ff829079
stefany <stefany.ribeiro4@aluno.ce.gov.br>
2026-08-21 19:00:54 +0000

    feito

---
+++ 3-bimestre/orientacao-a-objetos/aula01/index.js
@@ -0,0 +1,11 @@
+const Filme = {
+    titulo: "Interestelar",
+    diretor: "Christopher Nolan",
+    ano: 2014,
+
+    apresentar() {
+        return "Filme: Interestelar"
+    }
+
+}
+module.exports = Filme
